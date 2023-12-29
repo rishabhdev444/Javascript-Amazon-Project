1bf26baffe4813eda7c905df0cfca51df0fb9e15
Rishabh Dev <rishabhdev444@gmail.com>
2023-12-29 14:15:27 +0530

    Added To Cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product)=>{
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -83,5 +83,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         });
        
         document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
-    })
+
+        const addedMsg=document.querySelector(`.js-added-to-cart-${productId}`);
+
+        addedMsg.classList.add('added-to-cart-visible');
+
+        setTimeout(()=>{
+            addedMsg.classList.remove('added-to-cart-visible');
+        },2000);
+    });
 });
