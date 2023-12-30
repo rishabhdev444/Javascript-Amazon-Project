4bb9d147d1d63b5c8ce72d009703d80a3c99baf8
Rishabh Dev <rishabhdev444@gmail.com>
2023-12-30 11:31:58 +0530

    save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions=[{
+    id:'1',
+    deliveyDays:7,
+    priceCents:0
+},{
+    id:'2',
+    deliveyDays:3,
+    priceCents:499
+},{
+    id:'3',
+    deliveyDays:1,
+    priceCents:999
+}];
